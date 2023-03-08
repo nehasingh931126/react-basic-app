be632990f8f16f6f260899c21577012c36a99d3b
neha singh <neha.singh931126@gmail.com>
2023-03-08 12:09:54 +0530

    Added Validations to the two fields in the AddUser

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -7,7 +7,15 @@ const AddUser = (prop) => {
     const [enterAge, setEnteredAge] = useState('');
     const addUserHandlerFunction = (event) => {
         event.preventDefault();
+        if(enterUsername.trim().length ===0 || enterAge.trim().length===0) {
+            return;
+        }
+        if (+enterAge < 1) {
+            return;
+        }
         console.log(enterUsername, enterAge);
+        setEnteredUsername('');
+        setEnteredAge('');
     }
 
     const usernameChangeHandler = (event)=> {
@@ -25,9 +33,9 @@ const AddUser = (prop) => {
         <Card className={classes.input}>
             <form onSubmit={addUserHandlerFunction}>
                 <label htmlFor="username">Username</label>
-                <input id="username" type="text" onChange={usernameChangeHandler}/>
+                <input id="username" type="text" value={enterUsername} onChange={usernameChangeHandler}/>
                 <label htmlFor="age">Age(Years)</label>
-                <input id="age" type="number" onChange={ageChangeHandler} />
+                <input id="age" type="number" value={enterAge}  onChange={ageChangeHandler} />
                 <Button type="submit">Add User</Button>
             </form>
         </Card>)
